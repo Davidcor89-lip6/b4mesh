583ecaa497ef3c95e4933a1f4cd412c5fafe8961
GuillaumeDua4Squad <77145808+GuillaumeDuaForSquad@users.noreply.github.com>
2021-02-05 15:04:17 +0100

    Revert "CMake integration"

---
+++ include/application_packet.hpp
@@ -0,0 +1,64 @@
+#ifndef APPLICATION_PACKET_H
+#define APPLICATION_PACKET_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ApplicationPacket{
+  public:
+    // Typedef
+    typedef struct packet_hdr_serie{
+      int size;
+      char service;
+    } packet_hdr_serie;
+
+  public:
+    // Constants
+    enum {NONE, RAFT, PBFT, BLOCKGRAPH, RAFTB4M, TRANSACTION, BLOCK, REQUEST_BLOCK, CHANGE_TOPO};
+  public:
+    //Constructors and destructor
+    ApplicationPacket();
+    ApplicationPacket(char service, string payload);
+    ApplicationPacket(char service, int size, const uint8_t* payload);
+    ApplicationPacket(ApplicationPacket &p);
+    ApplicationPacket(string &serie);
+    ApplicationPacket(string &serie, size_t p_size);
+    ~ApplicationPacket();
+
+  public:
+    // Getters, Setters and Operators;
+    void SetSize(int size);
+    int GetSize();
+    void SetService(char service);
+    char GetService();
+    void SetPayload(string payload);
+    string GetPayload();
+
+    ostream& operator<<(const ostream& o);
+    friend ostream& operator<<(ostream& o, const ApplicationPacket& p);
+  public:
+    /**
+     * Calculate and return the total size of the packet
+     */
+    int CalculateSize();
+
+    /**
+     * Calculate and return the size of the header of the packet
+     */
+    int HeaderSize();
+
+    /**
+     * Serialize the packet into an array of byte (here a string is more
+     * convenient)
+     */
+     string Serialize();
+
+  private:
+    char service; // Service identifier of the packet (PBFT, RAFT, BLOCKCHAIN, ...)
+    int size; // Size of the whole packet (including header)
+    string payload; // Payload of the packet (contains an application message
+
+};
+
+#endif
---
+++ include/transaction.hpp
@@ -0,0 +1,69 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+#include <string.h>
+
+#include "utils.hpp"
+
+using namespace std;
+
+class Transaction{
+
+  public:
+    static const int HASH_SIZE = 20;
+
+  public:
+    typedef struct transaction_t{
+      double    timestamp;
+      int       size;
+      char    hash[HASH_SIZE];
+    } tx_t;
+
+  public:
+    //constructor and destructor
+    Transaction();
+    Transaction (string hash, int size, string payload, double timestamp);
+    Transaction (const Transaction &tx);
+    Transaction (const string& serie);
+    ~Transaction();
+
+  public:
+    //Getters, Setters & Operators
+    string GetHash (void);
+    void SetHash (string hash);
+
+    int GetSize (void);
+    void SetSize (int size);
+
+    string GetPayload (void);
+    void SetPayload (string payload);
+
+    double GetTimestamp (void);
+    void SetTimestamp (double timestamp);
+
+
+    bool operator==(const Transaction &tx);
+    friend std::ostream& operator<< (std::ostream &out, const Transaction &tx);
+    Transaction& operator=(const Transaction &tx2);
+
+  public:
+    string Serialize();
+    int CalculateSize();
+    int CalculateHeaderSize();
+    string CalculateHash();
+
+  public:
+    // Other functions
+
+  private:
+
+    string    hash;           // Identifier of a Tx
+    int       size;           // Size of the transaction
+    double    timestamp;   // Time when a transaction is created
+    string    payload;        // data of the transaction
+};
+
+#endif
