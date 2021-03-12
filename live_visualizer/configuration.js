7ff5e1e808c4990956d9cb812e28050cea81069a
Guillaume Dua @ Squad <gdua@squad.fr>
2021-03-12 14:50:26 +0100

    [b4mesh::live_visualizer] : live demo example

--- live_visualizer/configuration.js
+++ live_visualizer/configuration.js
@@ -1,4 +1,5 @@
 const configuration = {
-    blockgraph_as_dot_API_accesspoint: "http://127.0.0.1:8000/tests/datas/blockgraph.dot.json",
+    //blockgraph_as_dot_API_accesspoint: "http://127.0.0.1:8000/tests/datas/blockgraph.dot.json",
+    blockgraph_as_dot_API_accesspoint: "http://127.0.0.1:8000/tests/datas/tmp/TestDatas.json",
     refresh_rate_ms: 5000
 }
