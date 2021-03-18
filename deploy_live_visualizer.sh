d30a27ef9efc2f011299822e9e8ab513e81f55ad
Guillaume Dua @ Squad <gdua@squad.fr>
2021-03-18 14:48:20 +0100

    [b4mesh] : WIP : add (sources) : fix directory creation

--- deploy_live_visualizer.sh
+++ deploy_live_visualizer.sh
@@ -5,7 +5,8 @@ List_IP=("10.181.178.217" "10.181.172.130" "10.154.134.26" "10.154.134.170" "10.
 for addr in "${List_IP[@]}"
 do
     echo $addr
-    sshpass -p 'b4meshroot' scp CMakeLists.txt default@$addr:/var/persistent-data/b4mesh_src/
-    sshpass -p 'b4meshroot' scp live_visualizer default@$addr:/var/persistent-data/b4mesh_src/
-    sshpass -p 'b4meshroot' scp deploy default@$addr:/var/persistent-data/b4mesh_src/
+    sshpass -p 'b4meshroot' ssh default@$addr: "mkdir -p /var/persistent-data/b4mesh/tmp/"
+    sshpass -p 'b4meshroot' scp CMakeLists.txt default@$addr:/var/persistent-data/b4mesh/tmp/
+    sshpass -p 'b4meshroot' scp live_visualizer/ default@$addr:/var/persistent-data/b4mesh/tmp/
+    sshpass -p 'b4meshroot' scp deploy/ default@$addr:/var/persistent-data/b4mesh/tmp/
 done
