08143e7ff6d27829317da2a4e7ad3ef6196a5b15
Guillaume Dua @ Squad <gdua@squad.fr>
2021-03-19 15:45:28 +0100

    [b4mesh::live_visualizer] : WIP : deployement refactoring : live_visualizer/configuration.js generation

--- cmake_modules/get_nodes_filestream_path.cmake
+++ cmake_modules/get_nodes_filestream_path.cmake
@@ -11,8 +11,8 @@ function (get_nodes_filestream_path)
     string(REGEX REPLACE ${filestream_path_regex}
        "\\1" result
        "${result}")
-    if (result MATCHES "")
-        message(FATAL "get_nodes_filestream_path : Cannot find LIVEBLOCK_FILE compile-time constant in ${target_file}")
+    if (result STREQUAL "")
+        message(FATAL_ERROR "get_nodes_filestream_path : Cannot find LIVEBLOCK_FILE compile-time constant in ${target_file}")
     endif()
     set (nodes_filestream_path ${result} PARENT_SCOPE)
 endfunction()
